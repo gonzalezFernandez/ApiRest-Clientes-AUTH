0625dfdb6735c9779f795d974570a9edfce27649
gonzalezFernandez <andres.gonzalez-fernandez@capgemini.com>
2022-03-23 10:20:33 +0100

    FINAL OK

--- apirest-clientes/src/main/java/com/example/demo/apirest/controller/ClienteRestController.java
+++ apirest-clientes/src/main/java/com/example/demo/apirest/controller/ClienteRestController.java
@@ -18,6 +18,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -34,7 +34,7 @@ import com.example.demo.apirest.entity.Cliente;
 import com.example.demo.apirest.entity.Region;
 import com.example.demo.apirest.service.ClienteService;
 
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
@@ -258,7 +258,7 @@ public class ClienteRestController {
 		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
 	}
 	
-	@GetMapping("/uploads/imagen/{nombreImagen:.+}")
+	@GetMapping("/uploads/images/{nombreImagen:.+}")
 	public ResponseEntity<Resource> verImagen(@PathVariable String nombreImagen){
 		
 		Path rutaImagen = Paths.get("uploads").resolve(nombreImagen).toAbsolutePath();
